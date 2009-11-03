9cf5202273b012535d73a509bf13ffc97474d153
Abraham Williams <abraham@abrah.am>
2009-11-03 04:28:21 -0500

    added config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define("CONSUMER_KEY", "CONSUMER_KEY_GOES_HERE");
+define("CONSUMER_SECRET", "CONSUMER_SECRET_GOES_HERE");
