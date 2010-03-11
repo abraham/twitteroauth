32926ada4605bc5f3b4fea6b70d22e58650348cd
abraham <4braham@gmail.com>
2010-03-10 18:48:55 -0800

    formating and text changes

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define("CONSUMER_KEY", "");
-define("CONSUMER_SECRET", "");
-define("OAUTH_CALLBACK", "");
+define('CONSUMER_KEY', 'ELi0Wc1kMAuJyHJQ68AEg');
+define('CONSUMER_SECRET', 'Vkf6oqNnckF3Tlf00BVzQkMYmcx8mkkI7MzpXixsjY');
+define('OAUTH_CALLBACK', 'http://local.dev/twitteroauth/callback.php');
--- redirect.php
+++ redirect.php
@@ -5,17 +5,17 @@ session_start();
 require_once('twitteroauth/twitteroauth.php');
 require_once('config.php');
 
-/* Create TwitterOAuth object and get request token */
+/* Build TwitterOAuth object with client credentials. */
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
  
-/* Get request token. */
+/* Get temporary credentials. */
 $request_token = $connection->getRequestToken(OAUTH_CALLBACK);
 
-/* Save request token to session. */
+/* Save temporary credentials to session. */
 $_SESSION['oauth_token'] = $token = $request_token['oauth_token'];
 $_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
  
-/* If last connection fails don't display authorization link. */
+/* If last connection failed don't display authorization link. */
 switch ($connection->http_code) {
   case 200:
     /* Build authorize URL and redirect user to Twitter. */
