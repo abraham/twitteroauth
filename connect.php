bfebd2f0c110241eeea4a79e0cd160b7b2e0802f
abraham <4braham@gmail.com>
2010-03-10 16:26:52 -0800

    added a check in the example code to show an error if CONSUMER key/secret is not set.

--- connect.php
+++ connect.php
@@ -5,6 +5,16 @@
  * Get a request token from twitter and present authorization URL to user
  */
 
+/**
+ * Exit with an error message if the CONSUMER_KEY or CONSUMER_SECRET is not defined.
+ */
+require_once('config.php');
+if (CONSUMER_KEY === '' || CONSUMER_SECRET === '') {
+  echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://twitter.com/apps">https://twitter.com/apps</a>';
+  exit;
+}
+
+/* Build an image link to start the redirect process. */
 $content = '<a href="./redirect.php"><img src="./images/lighter.png" alt="Sign in with Twitter"/></a>';
  
 /* Include HTML to display on the page */
