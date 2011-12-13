aa7d7bc27c919af29475bbe7b599b155eed0d41a
Abraham Williams <4braham@gmail.com>
2011-12-13 18:04:25 -0500

    Replaced revoked keys with placeholder text.

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'ELi0Wc1kMAuJyHJQ68AEg');
-define('CONSUMER_SECRET', 'Vkf6oqNnckF3Tlf00BVzQkMYmcx8mkkI7MzpXixsjY');
-define('OAUTH_CALLBACK', 'http://local.dev/twitteroauth/callback.php');
+define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
+define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
