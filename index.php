3b998202049bcd9599a79fe5111611cafa716d57
MuhsinMuhammad <muhammadmuhsin31@yahoo.com>
2012-03-22 14:53:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,29 @@
 <?php
-/**
- * @file
- * User has successfully authenticated with Twitter. Access tokens saved to session and DB.
- */
-
-/* Load required lib files. */
-session_start();
-require_once('twitteroauth/twitteroauth.php');
-require_once('config.php');
-
+ 
+session_start ();
+require_once ('twitteroauth/twitteroauth.php');
+require_once ('config.php');
+ 
 /* If access tokens are not available redirect to connect page. */
-if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
-    header('Location: ./clearsessions.php');
+if (empty ($_SESSION['access_token']) || empty ($_SESSION['access_token']['oauth_token']) || empty ($_SESSION['access_token']['oauth_token_secret'])) {
+    //header('Location: ./clearsessions.php');
+    //tampilkan link OAuth
+?>
+<a href="./redirect.php"><img src="./images/lighter.png" alt="Sign in with Twitter"/></a>
+<?php
 }
-/* Get user access tokens out of the session. */
-$access_token = $_SESSION['access_token'];
+else
+{
+    //wah, sudah berhasil tersambung nih. Lakukan proses disini
+    $access_token = $_SESSION['access_token'];
+ 
+    $connection = new TwitterOAuth (CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+ 
+    $content = $connection->get ('account/verify_credentials');
+ 
+    var_dump ($content);
+}
 
-/* Create a TwitterOauth object with consumer/user tokens. */
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+Read more: http://bayu.freelancer.web.id/2010/10/20/cara-membuat-koneksi-ke-twitter-menggunakan-oauth-dan-php/#ixzz1ppHRasgl
+Under Creative Commons License: Attribution
 
-/* If method is set change API call made. Test is called by default. */
-$content = $connection->get('account/verify_credentials');
-
-/* Some example calls */
-//$connection->get('users/show', array('screen_name' => 'abraham'));
-//$connection->post('statuses/update', array('status' => date(DATE_RFC822)));
-//$connection->post('statuses/destroy', array('id' => 5437877770));
-//$connection->post('friendships/create', array('id' => 9436992));
-//$connection->post('friendships/destroy', array('id' => 9436992));
-
-/* Include HTML to display on the page */
-include('html.inc');
