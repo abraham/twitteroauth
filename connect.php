07860cee4f7546d3e044f2b973c48ef245183dac
Gopi Ramena <gopi@iitk.ac.in>
2012-03-31 16:23:30 +0530

    Added default of consumer key and secret as present in config.php by default.

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
  * Exit with an error message if the CONSUMER_KEY or CONSUMER_SECRET is not defined.
  */
 require_once('config.php');
-if (CONSUMER_KEY === '' || CONSUMER_SECRET === '') {
+if (CONSUMER_KEY === '' || CONSUMER_SECRET === '' || CONSUMER_KEY === 'CONSUMER_KEY_HERE' || CONSUMER_SECRET === 'CONSUMER_SECRET_HERE') {
   echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://twitter.com/apps">https://twitter.com/apps</a>';
   exit;
 }
