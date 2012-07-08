c11881c26d72856d6f14dadacc0ba488cb49398e
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2012-07-08 17:54:49 -0300

    Configuration removed

--- sample/config.php
+++ sample/config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'iMZxhk3GKr7fJBMIdM0L9w');
-define('CONSUMER_SECRET', 'PzNIsHJnKS2OO6qNkKyOlcNh1nZzHODa13BHkKDgLg');
-define('OAUTH_CALLBACK', 'http://127.0.0.1/workspace/git-repos/twitter-oauth/sample/callback.php');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', '');
