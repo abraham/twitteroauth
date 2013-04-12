61f5a550cd3643c619e84cb914ef1f22e491c270
Abraham Williams <4braham@gmail.com>
2013-04-11 21:00:00 -0700

    Updated twitter apps link

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@
  */
 require_once('config.php');
 if (CONSUMER_KEY === '' || CONSUMER_SECRET === '' || CONSUMER_KEY === 'CONSUMER_KEY_HERE' || CONSUMER_SECRET === 'CONSUMER_SECRET_HERE') {
-  echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://twitter.com/apps">https://twitter.com/apps</a>';
+  echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://dev.twitter.com/apps">dev.twitter.com/apps</a>';
   exit;
 }
 
