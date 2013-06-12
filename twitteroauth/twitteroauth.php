7d212801ca9049d8e492b2c33e287cc9b4f713b4
floodlightdesign <joe@floodlightdesign.com>
2013-06-12 09:53:35 -0400

    Updated to use twitter 1.1 API
    Just updated the endpoint to use 1.1 API

--- twitteroauth/twitteroauth.php
+++ twitteroauth/twitteroauth.php
@@ -18,7 +18,7 @@ class TwitterOAuth {
   /* Contains the last API call. */
   public $url;
   /* Set up the API root URL. */
-  public $host = "https://api.twitter.com/1/";
+  public $host = "https://api.twitter.com/1.1/";
   /* Set timeout default. */
   public $timeout = 30;
   /* Set connect timeout. */
