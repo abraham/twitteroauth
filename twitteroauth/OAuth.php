906f7f13f2d489b8554110d72142753c0789fb62
Oliver Nassar <onassar@gmail.com>
2014-02-08 17:12:31 -0500

    Update OAuth.php
    Conditional to check port existence

--- twitteroauth/OAuth.php
+++ twitteroauth/OAuth.php
@@ -382,7 +382,7 @@ class OAuthRequest {
   public function get_normalized_http_url() {
     $parts = parse_url($this->http_url);
 
-    $port = @$parts['port'];
+    $port = isset($parts['port']) ? @$parts['port'] : false;
     $scheme = $parts['scheme'];
     $host = $parts['host'];
     $path = @$parts['path'];
