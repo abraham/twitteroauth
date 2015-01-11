03336cfb70c1c91bfebb96f43d12041bf63ca666
louis <louis@hugin-2.darkside>
2015-01-11 18:46:27 +0100

    add tests for Consumer, Token, HmacSha1 & Plaintext

--- tests/TwitterOAuthTest.php
+++ tests/TwitterOAuthTest.php
@@ -6,15 +6,6 @@ namespace Abraham\TwitterOAuth\Test;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-define('CONSUMER_KEY', getenv('TEST_CONSUMER_KEY'));
-define('CONSUMER_SECRET', getenv('TEST_CONSUMER_SECRET'));
-define('ACCESS_TOKEN', getenv('TEST_ACCESS_TOKEN'));
-define('ACCESS_TOKEN_SECRET', getenv('TEST_ACCESS_TOKEN_SECRET'));
-define('OAUTH_CALLBACK', getenv('TEST_OAUTH_CALLBACK'));
-define('PROXY', getenv('TEST_CURLOPT_PROXY'));
-define('PROXYUSERPWD', getenv('TEST_CURLOPT_PROXYUSERPWD'));
-define('PROXYPORT', getenv('TEST_CURLOPT_PROXYPORT'));
-
 class TwitterOAuthTest extends \PHPUnit_Framework_TestCase
 {
     /** @var TwitterOAuth */
