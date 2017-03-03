7f2f579385a60415f66b02d408814aa254028d04
Gabriel Ostrolucký <gadelat@gmail.com>
2017-03-03 10:45:11 +0100

    Allow to override request method
    This allows the user to add things such as logging of requests/responses and replace CURL with something else, e.g. Guzzle

--- src/TwitterOAuth.php
+++ src/TwitterOAuth.php
@@ -364,7 +364,7 @@ class TwitterOAuth extends Config
      * @return string
      * @throws TwitterOAuthException
      */
-    private function request($url, $method, $authorization, array $postfields)
+    protected function request($url, $method, $authorization, array $postfields)
     {
         /* Curl settings */
         $options = [
