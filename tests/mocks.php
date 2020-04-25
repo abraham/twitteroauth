71e29ba7cee789f0893ec3db5cfd5d6870f5a138
Abraham Williams <4braham@gmail.com>
2020-04-25 15:23:53 -0500

    Add VCR

---
+++ tests/mocks.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Abraham\TwitterOAuth;
+
+// Mock time and random values for consistent tests with VCR
+function time()
+{
+    return MOCK_TIME;
+}
+
+function microtime()
+{
+    return 'FAKE_MICROTIME';
+}
+
+function mt_rand()
+{
+    return 123456789;
+}
