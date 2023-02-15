b2ea17b6861b793af854528c737a78579a220ab5
neopage <neopage@users.noreply.github.com>
2023-02-15 10:19:48 +0900

    Update TwitterOAuth.php to upload image using URL
    remove:  !is_readable($parameters['media'])

--- src/TwitterOAuth.php
+++ src/TwitterOAuth.php
@@ -336,10 +336,7 @@ class TwitterOAuth extends Config
      */
     private function uploadMediaNotChunked(string $path, array $parameters)
     {
-        if (
-            !is_readable($parameters['media']) ||
-            ($file = file_get_contents($parameters['media'])) === false
-        ) {
+        if ( ($file = file_get_contents($parameters['media'])) === false ) {
             throw new \InvalidArgumentException(
                 'You must supply a readable file',
             );
